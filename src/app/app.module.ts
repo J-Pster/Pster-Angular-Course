ba95816402b58288ffb2e72d9633348661b74258
João Pster <joaopsterdev@gmail.com>
2022-12-08 16:59:25 -0300

    feat(two-way-data): Aprendendo binding de formulários no Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
@@ -11,6 +12,7 @@ import { EventsComponent } from './components/events/events.component';
 import { NumberShowComponent } from './components/number/number-show/number-show.component';
 import { NumberChangerComponent } from './components/number/number-changer/number-changer.component';
 import { ListDevelopersComponent } from './components/list-developers/list-developers.component';
+import { TwoWayDataComponent } from './components/two-way-data/two-way-data.component';
 
 @NgModule({
   declarations: [
@@ -24,8 +26,9 @@ import { ListDevelopersComponent } from './components/list-developers/list-devel
     NumberShowComponent,
     NumberChangerComponent,
     ListDevelopersComponent,
+    TwoWayDataComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/two-way-data/two-way-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-data',
+  templateUrl: './two-way-data.component.html',
+  styleUrls: ['./two-way-data.component.scss'],
+})
+export class TwoWayDataComponent {
+  name: string = '';
+}
