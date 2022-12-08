a4b0ceef3f914d65d2849982aab60ac1f1d45aab
João Pster <joaopsterdev@gmail.com>
2022-12-08 16:48:58 -0300

    feat(pipe-operators): Aprendendo Pipe operators e implementado formatação de data

--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+registerLocaleData(localePT);
 
 @Component({
   selector: 'app-parent-data',
   templateUrl: './parent-data.component.html',
   styleUrls: ['./parent-data.component.scss'],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-br' }],
 })
 export class ParentDataComponent {
   @Input() currentData!: Date;
