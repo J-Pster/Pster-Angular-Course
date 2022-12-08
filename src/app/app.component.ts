0bf25929af459a85bf8ca6202e8a5c86888523c1
João Pster <joaopsterdev@gmail.com>
2022-12-08 14:55:22 -0300

    feat(data): Aprendendo compartilhamento de dados entre componentes parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'pster-angular-course';
+  currentData: Date = new Date();
+  appVersion: string = '1.0.0';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    ProfileCardComponent
+    ProfileCardComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule
